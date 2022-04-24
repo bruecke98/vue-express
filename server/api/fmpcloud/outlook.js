6023f0715d7c81f59ce86bd5ac52520100d6f558
bruecke98 <brueckmann.paul@gmail.com>
2022-04-24 19:37:31 +0200

    API

--- server/api/fmpcloud/outlook.js
+++ server/api/fmpcloud/outlook.js
@@ -2,6 +2,8 @@ const axios = require("axios");
 const { env } = require("process");
 
 
+//API für fmpcloud
+
 let getData = async (symbol) => {
     let response = await axios(`https://fmpcloud.io/api/v4/company-outlook?symbol=${symbol.id.toUpperCase()}&apikey=${process.env.API_KEY}`);
     return response;
